354c52388ce5627f8afeb54c077e6f82d2a1b47e
ghosind <ghosind@gmail.com>
2021-03-03 23:47:22 +0800

    remove static

--- day3/bootpack.c
+++ day3/bootpack.c
@@ -1,7 +1,8 @@
 void io_hlt(void);
 
 // 入口函数HariMain重命名为标准的main
-void main(void) {
+// 返回类型修改为int，避免编译器警告
+int main(void) {
 fin:
   io_hlt();
   goto fin;
--- day4/func.asm
+++ day4/func.asm
@@ -9,71 +9,71 @@
   ; GLOBAL write_mem8
 
   [SECTION .text]
-io_hlt:              ; void io_hlt(void);
+io_hlt:                 ; void io_hlt(void);
   HLT
   RET
 
-io_cli:
+io_cli:                 ; void io_cli(void);
   CLI
   RET
 
-io_sti:
+io_sti:                 ; void io_sti(void);
   STI
   RET
 
-io_stihlt:
+io_stihlt:              ; void io_stihlt(void);
   STI
   HLT
   RET
 
-io_in8:
-  MOV   EDX, [ESP+4]
-  MOV   EAX, 0
-  IN    AL, DX
+io_in8:                 ; int io_in8(int port);
+  MOV     EDX, [ESP+4]  ; port
+  MOV     EAX, 0
+  IN      AL, DX
   RET
 
-io_in16:
-  MOV   EDX, [ESP+4]
-  MOV   EAX, 0
-  IN    AX, DX
+io_in16:                ; int io_in16(int port);
+  MOV     EDX, [ESP+4]  ; port
+  MOV     EAX, 0
+  IN      AX, DX
   RET
 
-io_in32:
-  MOV   EDX, [ESP+4]
-  IN    EAX, DX
+io_in32:                ; int io_in32(int port);
+  MOV     EDX, [ESP+4]  ; port
+  IN      EAX, DX
   RET
 
-io_out8:
-  MOV   EDX, [ESP+4]
-  MOV   AL, [ESP+8]
-  OUT   DX, AL
+io_out8:                ; void io_out8(int port, int data);
+  MOV     EDX, [ESP+4]  ; port
+  MOV     AL, [ESP+8]   ; data
+  OUT     DX, AL
   RET
 
-io_out16:
-  MOV   EDX, [ESP+4]
-  MOV   AX, [ESP+8]
-  OUT   DX, AX
+io_out16:               ; void io_out16(int port, int data);
+  MOV     EDX, [ESP+4]  ; port
+  MOV     AX, [ESP+8]   ; data
+  OUT     DX, AX
   RET
 
-io_out32:
-  MOV   EDX, [ESP+4]
-  MOV   EAX, [ESP+8]
-  OUT   DX, EAX
+io_out32:               ; void io_out32(int port, int data);
+  MOV     EDX, [ESP+4]  ; port
+  MOV     EAX, [ESP+8]  ; data
+  OUT     DX, EAX
   RET
 
-io_load_eflags:
+io_load_eflags:         ; int io_load_eflags(void);
   PUSHFD
-  POP   EAX
+  POP     EAX
   RET
 
-io_store_eflags:
-  MOV   EAX, [ESP+4]
-  PUSH  EAX
+io_store_eflags:        ; void io_store_eflags(int eflags);
+  MOV     EAX, [ESP+4]  ; eflags
+  PUSH    EAX
   POPFD
   RET
 
-; write_mem8:
-;   MOV   ECX, [ESP+4]
-;   MOV   AL, [ESP+8]
-;   MOV   [ECX], AL
+; write_mem8:             ; void write_mem8(int addr, int data);
+;   MOV     ECX, [ESP+4]  ; addr
+;   MOV     AL, [ESP+8]   ; data
+;   MOV     [ECX], AL
 ;   RET
