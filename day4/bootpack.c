8f0a0ee6f69f9566c53eb5d887a5f02e7ec38924
ghosind <ghosind@gmail.com>
2021-03-04 00:09:12 +0800

    day4 done.

--- day4/bootpack.c
+++ day4/bootpack.c
@@ -8,23 +8,73 @@ void io_store_eflags(int eflags);
 
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
+void box_fill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+
+#define COL8_000000   0
+#define COL8_FF0000   1
+#define COL8_00FF00   2
+#define COL8_FFFF00   3
+#define COL8_0000FF   4
+#define COL8_FF00FF   5
+#define COL8_00FFFF   6
+#define COL8_FFFFFF   7
+#define COL8_C6C6C6   8
+#define COL8_840000   9
+#define COL8_008400   10
+#define COL8_848400   11
+#define COL8_000084   12
+#define COL8_840084   13
+#define COL8_008484   14
+#define COL8_848484   15
 
 // 入口函数HariMain重命名为标准的main
 // 返回类型修改为int，避免编译器警告
 int main(void) {
-  char *p = (char *) 0xa0000;
+  unsigned char *vram = (unsigned char *) 0xa0000;
+  int xSize = 320, ySize = 200;
 
   init_palette();
 
-  for (int i = 0; i <= 0xffff; i++) {
-    // write_mem8(i, i & 0x0f);
-    *(p + i) = i & 0x0f;
-  }
+  box_fill8(vram, xSize, COL8_008484, 0,          0,          xSize - 1,  ySize - 29);
+  box_fill8(vram, xSize, COL8_C6C6C6, 0,          ySize - 28, xSize - 1,  ySize - 28);
+  box_fill8(vram, xSize, COL8_FFFFFF, 0,          ySize - 27, xSize - 1,  ySize - 27);
+  box_fill8(vram, xSize, COL8_C6C6C6, 0,          ySize - 26, xSize - 1,  ySize - 1);
+
+  box_fill8(vram, xSize, COL8_FFFFFF, 3,          ySize - 24, 59,         ySize - 24);
+  box_fill8(vram, xSize, COL8_FFFFFF, 2,          ySize - 24, 2,          ySize - 4);
+  box_fill8(vram, xSize, COL8_848484, 3,          ySize - 4,  59,         ySize - 4);
+  box_fill8(vram, xSize, COL8_848484, 59,         ySize - 23, 59,         ySize - 5);
+  box_fill8(vram, xSize, COL8_000000, 2,          ySize - 4,  59,         ySize - 3);
+  box_fill8(vram, xSize, COL8_000000, 60,         ySize - 24, 60,         ySize - 3);
+
+  box_fill8(vram, xSize, COL8_848484, xSize - 47, ySize - 24, xSize - 4,  ySize - 24);
+  box_fill8(vram, xSize, COL8_848484, xSize - 47, ySize - 23, xSize - 47, ySize - 4);
+  box_fill8(vram, xSize, COL8_FFFFFF, xSize - 47, ySize - 3,  xSize - 4,  ySize - 3);
+  box_fill8(vram, xSize, COL8_FFFFFF, xSize - 3,  ySize - 24, xSize - 3,  ySize - 3);
 
   for (;;) {
     io_hlt();
   }
 }
+
+// int main(void) {
+//   unsigned char *p = (unsigned char *) 0xa0000;
+
+//   init_palette();
+
+//   for (int i = 0; i <= 0xffff; i++) {
+//     // write_mem8(i, i & 0x0f);
+//     *(p + i) = i & 0x0f;
+//   }
+
+//   box_fill8(p, 320, COL8_FF0000, 20, 20, 120, 120);
+//   box_fill8(p, 320, COL8_00FF00, 70, 50, 170, 150);
+//   box_fill8(p, 320, COL8_0000FF, 120, 80, 220, 180);
+
+//   for (;;) {
+//     io_hlt();
+//   }
+// }
 
 void init_palette(void) {
   // 修改后无法正常读取到静态变量，所以将static移除
@@ -67,6 +117,16 @@ void set_palette(int start, int end, unsigned char *rgb) {
   }
 
   io_store_eflags(eflags);
+
+  return;
+}
+
+void box_fill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1) {
+  for (int y = y0; y <= y1; y++) {
+    for (int x = x0; x <= x1; x++) {
+      vram[y * xsize + x] = c;
+    }
+  }
 
   return;
 }
