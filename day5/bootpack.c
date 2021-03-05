5212962f9b9dee5fb320f977213c444babd6eb83
ghosind <ghosind@gmail.com>
2021-03-05 23:51:19 +0800

    Rename func.asm to io.asm, and add io.h.

--- day5/bootpack.c
+++ day5/bootpack.c
@@ -1,9 +1,4 @@
-void io_hlt(void);
-void io_cli(void);
-void io_sti(void);
-void io_out8(int port, int data);
-int io_load_eflags(void);
-void io_store_eflags(int eflags);
+#include "io.h"
 
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
