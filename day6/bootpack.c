f381d858073539d6b4a90837d1e7012b01ea7082
ghosind <ghosind@gmail.com>
2021-03-11 23:43:34 +0800

    Add libc.

--- day6/bootpack.c
+++ day6/bootpack.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "bootpack.h"
 #include "desctbl.h"
 #include "graphic.h"
@@ -7,6 +9,7 @@
 int main(void) {
   struct BootInfo *binfo = (struct BootInfo *) ADR_BOOTINFO;
   char mcursor[256];
+  char s[40];
 
   init_gdtidt();
   init_pic(); // GDT/IDT完成初始化，开放CPU中断
@@ -20,6 +23,8 @@ int main(void) {
 	int my = (binfo->scrny - 28 - 16) / 2;
 	init_mouse_cursor8(mcursor, COL8_008484);
 	put_block8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+  sprintf(s, "(%d, %d)", mx, my);
+	put_fonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
   io_out8(PIC0_IMR, 0xf9); // 开放PIC1以及键盘中断
   io_out8(PIC1_IMR, 0xef); // 开放鼠标中断
