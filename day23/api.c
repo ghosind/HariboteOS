5337538cbf628be22799770fdb8864a67f0cf9f0
ghosind <ghosind@gmail.com>
2021-05-23 23:58:33 +0800

    Refactor hrb_api.

--- day23/api.c
+++ day23/api.c
@@ -17,15 +17,20 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx,
   char s[12];
   int *reg = &eax + 1;
 
-  if (edx == 1) {
+  switch (edx) {
+  case 1:
     cons_putchar(cons, eax & 0xff, 1);
-  } else if (edx == 2) {
+    break;
+  case 2:
     cons_putstr(cons, (char *)ebx + ds_base);
-  } else if (edx == 3) {
+    break;
+  case 3:
     cons_putnstr(cons, (char *)ebx + ds_base, ecx);
-  } else if (edx == 4) {
+    break;
+  case 4:
     return &(task->tss.esp0);
-  } else if (edx == 5) {
+    break;
+  case 5:
     sht = sheet_alloc(shtctl);
     sheet_setbuf(sht, (unsigned char *)(ebx + ds_base), esi, edi, eax);
     make_window8((unsigned char *)(ebx + ds_base), esi, edi,
@@ -33,38 +38,48 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx,
     sheet_slide(sht, 100, 50);
     sheet_updown(sht, 3);
     reg[7] = (int)sht;
-  } else if (edx == 6) {
+    break;
+  case 6:
     sht = (struct Sheet *)(ebx & 0xfffffffe);
     put_fonts8_asc(sht->buf, sht->bxsize, esi, edi, eax,
                    (char *)(ebp + ds_base));
     if (!(ebx & 1)) {
       sheet_refresh(sht, esi, edi, esi + ecx * 8, edi + 16);
     }
-  } else if (edx == 7) {
+    break;
+  case 7:
     sht = (struct Sheet *)(ebx & 0xfffffffe);
     box_fill8(sht->buf, sht->bxsize, ebp, eax, ecx, esi, edi);
     if (!(ebx & 1)) {
       sheet_refresh(sht, eax, ecx, esi + 1, edi + 1);
     }
-  } else if (edx == 8) {
+    break;
+  case 8:
     memman_init((struct MemMan *)(ebx + ds_base));
     ecx &= 0xfffffff0;
     memman_free((struct MemMan *)(ebx + ds_base), eax, ecx);
-  } else if (edx == 9) {
+    break;
+  case 9:
     ecx = (ecx + 0x0f) & 0xfffffff0;
     reg[7] = memman_alloc((struct MemMan *)(ebx + ds_base), ecx);
-  } else if (edx == 10) {
+    break;
+  case 10:
     ecx = (ecx + 0x0f) & 0xfffffff0;
     memman_free((struct MemMan *)(ebx + ds_base), eax, ecx);
-  } else if (edx == 11) {
+    break;
+  case 11:
     sht = (struct Sheet *)(ebx & 0xfffffffe);
     sht->buf[sht->bxsize * edi + esi] = eax;
     if (!(ebx & 1)) {
       sheet_refresh(sht, esi, edi, esi + 1, edi + 1);
     }
-  } else if (edx == 12) {
+    break;
+  case 12:
     sht = (struct Sheet *)ebx;
     sheet_refresh(sht, eax, ecx, esi, edi);
+    break;
+  default:
+    break;
   }
 
   return 0;
